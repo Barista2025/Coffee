d96ce863b724e22900fdca1c3162451df7cd8e41
Barista2025 <LeeWuResearch@gmail.com>
2025-03-28 00:05:42 +0800

    Update main.py
    add coffee cup detection

--- main.py
+++ main.py
@@ -6,27 +6,32 @@ import os
 import matplotlib as mpl
 mpl.use('Qt5Agg')
 import matplotlib.pyplot as plt
-import utility as util
 import talib
 import math
 
 from sklearn.metrics import confusion_matrix, classification_report
 
-# 假设 y_true 是真实标签，y_pred 是模型预测标签
-y_true = [1, 0, 1, 1, 0, 1, 0, 0, 1, 0]  # 实际值
-y_pred = [1, 0, 1, 0, 0, 1, 1, 0, 1, 0]  # 预测值
+def is_coffee_and_handle(STK,START,END):
+    price = yf.Ticker(STK).history(start=START,end=END, interval='1d').Close
+    date = yf.Ticker(STK).history(start=START,end=END, interval='1d').index
+    l = len(price)
+    for i in range(l-1,0,-1):
+        if price.iloc[i]>price.iloc[i-1]:
+            j = i-1 #pivot1
+            for k in range(j-1,0,-1):
+                if price.iloc[k]>price.iloc[k-1]:
+                    r = k #cup right point
+                    for p in range(r-1,0,-1):
+                        if price.iloc[p] >= price.iloc[r]:
+                            print(str(price.iloc[j])+" "+str(date[j]))
+                            fig,ax=plt.subplots()
+                            ax.plot(price.iloc[p:j],label='Price')
+                            ax.legend()
+                            plt.show()
+                            return True
+    return False
 
-# 计算混淆矩阵
-cm = confusion_matrix(y_true, y_pred)
-print("Confusion Matrix:\n", cm)
-
-price = yf.Ticker("AMBA").history(start="2017-06-01",end='2019-04-24', interval='1d').Close
-date = yf.Ticker("AMBA").history(start="2017-06-01",end='2019-04-24', interval='1d').index
-
-l = len(price)
-
-for i in range(l-2,0,-1):
-    if price.iloc[i] >= price.iloc[-1]:
-        break
-
-print(str(price.iloc[i])+" "+str(date[i]))
+dict = {'NVDA':('2015-12-30','2016-05-12')}
+stocks = ['NVDA']#,'AMBA']
+for stock in stocks:
+    print("Is "+stock+" a good coffee? "+str(is_coffee_and_handle(stock,dict[stock][0],dict[stock][1])))
