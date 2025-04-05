db22a092622970673697f16bcf6c43ad785ddc85
justinwu258 <justinwu258@users.noreply.github.com>
2025-04-06 00:33:04 +0800

    fix incorrect U type
    1. if the price higher than cup right, it's illegal
        break -> if the period < 35 days
    
    2. find highest cup right and mark with red point

--- main.py
+++ main.py
@@ -36,12 +36,26 @@ def is_coffee_and_handle(STK,START,END):
                     break
                 if price.iloc[k]>price.iloc[k-1]:
                     #print("p(k)="+str(price.iloc[k])+" period="+str(j-k)+" ratio="+str(price.iloc[k]/price.iloc[j]))
-                    print("Debug i:"+str(i)+" "+str(date[i].date()))
+                    #print("Debug i:"+str(i)+" "+str(date[i].date()))
                     r = k #cup right point
                     if ((j-r)>10) or ((handle_right-r)<5) or (price.iloc[r]>(price.iloc[handle_right]*1.12)):
                         continue
                     for p in range(r-1,-1,-1):
+                        if ((r-p)<lookback_window and (price.iloc[p] > price.iloc[r])):
+                            break
+                    
                         if ((r-p)>=lookback_window) and (price.iloc[p] > price.iloc[r]) and (price.iloc[p]<=(1.07*price.iloc[r])):
+                            originR = r
+                            tmpR = r
+                            max_R_idx = tmpR
+                            max_price_R = price.iloc[tmpR]
+                            while tmpR<handle_right:
+                                if price.iloc[tmpR]>max_price_R:
+                                    max_price_R = max(price.iloc[tmpR],max_price_R)
+                                    max_R_idx = tmpR
+                                tmpR += 1
+                            r = max_R_idx
+                        
                             left_idx = p
                             print("Found potential Cup and Handle")
                             print(f"Right edge price: {price.iloc[handle_right]} @ {date[handle_right].date()}")
@@ -49,6 +63,7 @@ def is_coffee_and_handle(STK,START,END):
                             print(str(price.iloc[handle_right])+" "+str(date[handle_right]))
                             fig,ax=plt.subplots()
                             ax.plot(price.iloc[left_idx:handle_right+1],label='Price')
+                            ax.plot(date[originR], price.iloc[originR], 'yo', label='Orginal Cup Right')
                             ax.plot(date[r], price.iloc[r], 'ro', label='Cup Right')
                             ax.plot(date[handle_right], price.iloc[handle_right], 'bo', label='Handle Right')
                             ax.plot(date[left_idx], price.iloc[left_idx], 'go', label='Cup Left')
@@ -70,6 +85,6 @@ def is_coffee_and_handle(STK,START,END):
     return False
 
 dict = {'AMBA':('2020-04-03','2025-04-03'),'NVDA':('2015-12-30','2022-10-14'),'META':('2020-04-03','2025-04-03'),'TSM':('2020-04-03','2025-04-03'),'SBUX':('2020-04-03','2025-04-03'),'MCD':('2020-04-03','2025-04-03'),'RIVN':('2020-04-03','2025-04-03'),'INTC':('2020-04-03','2025-04-03')}
-stocks = ['INTC']#'AMBA','NVDA','META']
+stocks = ['MCD']#'AMBA','NVDA','META']
 for stock in stocks:
     print("Is "+stock+" a good coffee? "+str(is_coffee_and_handle(stock,dict[stock][0],dict[stock][1])))
