16d01e2dd48a942a30c7689f4cd9cfba857e8251
Barista2025 <LeeWuResearch@gmail.com>
2025-04-10 20:21:39 +0800

    Add files via upload

---
+++ query_stocks.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import sys 
+import os
+import matplotlib as mpl 
+mpl.use('Qt5Agg')
+import matplotlib.pyplot as plt 
+import matplotlib.ticker as mticker
+import talib
+import math
+import time
+from utility import is_coffee_and_handle 
+
+fp = open("history/stock_list.txt","r")
+stocks = fp.read().split('\n')[:-1]
+for stock in stocks:
+    print(stock)
+    df = pd.read_csv('history/'+stock+'.csv')
+    is_coffee_and_handle(stock,df)
