259674a7249afc423c2735970b7f786ac0e20b0d
Barista2025 <LeeWuResearch@gmail.com>
2025-04-16 21:24:16 +0800

    Add operation mode
    Add operation mode

--- utility.py
+++ utility.py
@@ -1,11 +1,11 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
-import sys
+import sys 
 import os
-import matplotlib as mpl
+import matplotlib as mpl 
 mpl.use('Qt5Agg')
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.ticker as mticker
 import talib
 import math
@@ -13,23 +13,26 @@ import time
 
 from sklearn.metrics import confusion_matrix, classification_report
 
-def is_coffee_and_handle(STK,DF=None):
-    os.system("rm -rf image/"+STK)
-    os.system("mkdir -p image/"+STK)
+def is_coffee_and_handle(STK,DF=None,MODE=0):
+    if (MODE == 1) or (MODE == 3): 
+        os.system("rm -rf image/"+STK+"/*.png")
+    if MODE>=2:
+        os.system("rm -rf image/"+STK+"/report.txt")
     if DF is None:
         time.sleep(2)  # 每支股票之間等 2 秒
         price = yf.Ticker(STK).history(period='5y', interval='1d').Close
         vol = yf.Ticker(STK).history(period='5y', interval='1d').Volume
-        date = yf.Ticker(STK).history(period='5y', interval='1d').index
     else:
         price = DF.Close
         vol = DF.Volume
-        date = pd.to_datetime(DF.date)
+        price.index = pd.to_datetime(DF.Date,utc = True,format='ISO8601')
+        #price.index = DF.index.strftime("%d/%m/%Y")
     price50 = price.rolling(50).mean()
     l = len(price)
+    handle_arr = []
     next_cup = False
     cup_id = 1 
-    i = l-1
+    i = l-1 
     while i>0:
         if price.iloc[i]>price.iloc[i-1]:
             j = i-1 #pivot1
@@ -49,7 +52,7 @@ def is_coffee_and_handle(STK,DF=None):
                     for p in range(r-1,-1,-1):
                         if ((r-p)<lookback_window and (price.iloc[p] > price.iloc[r])):
                             break
-                    
+
                         if ((r-p)>=lookback_window) and (price.iloc[p] > price.iloc[r]) and (price.iloc[p]<=(1.07*price.iloc[r])):
                             originR = r
                             tmpR = r
@@ -81,22 +84,24 @@ def is_coffee_and_handle(STK,DF=None):
                                     inliner += 1
                             if (max_ratio>0.4) or ((inliner/(r-p-1))<0.05):
                                 break
-
                             left_idx = p
-                            fig,ax=plt.subplots()
-                            ax.plot(price.iloc[left_idx:handle_right+1],label='Price')
-                            ax.plot(date[originR], price.iloc[originR], 'yo', label='Orginal Cup Right')
-                            ax.plot(date[r], price.iloc[r], 'ro', label='Cup Right')
-                            ax.plot(date[handle_right], price.iloc[handle_right], 'bo', label='Handle Right')
-                            ax.plot(date[left_idx], price.iloc[left_idx], 'go', label='Cup Left')
-                            ax.legend()
-                            y_min = price.iloc[left_idx:handle_right+1].min()
-                            y_max = price.iloc[left_idx:handle_right+1].max()
-                            ax.set_ylim(y_min * 0.95, y_max * 1.10)  # 下壓 5%、上留 10%
-                            ax.yaxis.set_major_locator(mticker.MaxNLocator(nbins=30))  # 最多顯示 10 格
-                            plt.title(STK+" cup "+str(cup_id))
-                            plt.savefig('image/'+STK+'/cup'+str(cup_id)+'.png')
-                            plt.close()
+                            if MODE>=2:
+                                handle_arr.append(handle_right)
+                            if (MODE == 1) or (MODE == 3):
+                                fig,ax=plt.subplots()
+                                ax.plot(price.iloc[left_idx:handle_right+1],label='Price')
+                                ax.plot(price.index[originR], price.iloc[originR], 'yo', label='Orginal Cup Right')
+                                ax.plot(price.index[r], price.iloc[r], 'ro', label='Cup Right')
+                                ax.plot(price.index[handle_right], price.iloc[handle_right], 'bo', label='Handle Right')
+                                ax.plot(price.index[left_idx], price.iloc[left_idx], 'go', label='Cup Left')
+                                ax.legend()
+                                y_min = price.iloc[left_idx:handle_right+1].min()
+                                y_max = price.iloc[left_idx:handle_right+1].max()
+                                ax.set_ylim(y_min * 0.95, y_max * 1.10)  # 下壓 5%、上留 10%
+                                ax.yaxis.set_major_locator(mticker.MaxNLocator(nbins=30))  # 最多顯示 10 格
+                                plt.title(STK+" cup "+str(cup_id))
+                                plt.savefig('image/'+STK+'/cup'+str(cup_id)+'.png')
+                                plt.close()
                             cup_id += 1
                             next_cup = True
                             i = p
@@ -104,3 +109,5 @@ def is_coffee_and_handle(STK,DF=None):
                         if price.iloc[p]>(1.07*price.iloc[r]):
                             break
         i -= 1
+    if MODE>=2:
+        os.system("echo '"+str(handle_arr)+"' >> image/"+STK+"/report.txt")
