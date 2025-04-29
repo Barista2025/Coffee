ffe072e25086ea5dfa4eb29056459b360401ba5c
Barista2025 <LeeWuResearch@gmail.com>
2025-04-29 14:01:07 +0800

    Add files via upload

--- query_stocks.py
+++ query_stocks.py
@@ -4,7 +4,6 @@ import pandas as pd
 import sys 
 import os
 import matplotlib as mpl 
-mpl.use('Qt5Agg')
 import matplotlib.pyplot as plt 
 import matplotlib.ticker as mticker
 import talib
@@ -17,7 +16,11 @@ mode = int(sys.argv[2])
 fp = open("history/stock_list.txt","r")
 stocks = fp.read().split('\n')[:-1]
 os.system("mkdir -p "+folder)
+if mode == 0:
+    os.system("rm -rf "+folder+"/BAD")
+    os.system("mkdir -p "+folder+"/BAD")
+bad_cup_id = 1
 for stock in stocks:
     print(stock)
     df = pd.read_csv('history/'+stock+'.csv', date_format="%m/%d/%Y")
-    is_coffee_and_handle(stock,df,mode,folder)
+    bad_cup_id = is_coffee_and_handle(stock,df,mode,folder,bad_cup_id)
